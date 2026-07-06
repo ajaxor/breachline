4ff982130c5c4eefa2afdfaa6d9d386fbdac8c51
ajaxor <nathan.ferch@gmail.com>
2026-07-06 14:33:09 -0700

    Wire semantic combat presentation

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 import { GameController } from './backend/GameController.js';
 import { validateGameData } from './data/validateGameData.js';
 import { BattlefieldRenderer } from './frontend/BattlefieldRenderer.js';
+import { CombatEventPresenter } from './frontend/CombatEventPresenter.js';
 import { GameView } from './frontend/GameView.js';
 import { StrategyGameModel } from './model/StrategyGameModel.js';
 
 validateGameData();
 
 const view = new GameView();
-const model = new StrategyGameModel();
+const model = new StrategyGameModel({ eventPresenter: new CombatEventPresenter() });
 const renderer = new BattlefieldRenderer(view.elements.field, view.elements.fieldWrap);
 const buildInfo = window.BREACH_LINE_BUILD || { version: 'dev', commit: 'local' };
 const controller = new GameController(model, view, renderer, buildInfo);
