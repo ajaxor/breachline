0f5be95a4e3f37e655af15f777eac3d9eea4b4ca
ajaxor <nathan.ferch@gmail.com>
2026-07-06 11:14:51 -0700

    Use persistent strategy-layer game model

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import { GameController } from './backend/GameController.js';
 import { BattlefieldRenderer } from './frontend/BattlefieldRenderer.js';
 import { GameView } from './frontend/GameView.js';
-import { GameModel } from './model/GameModel.js';
+import { StrategyGameModel } from './model/StrategyGameModel.js';
 
 const view = new GameView();
-const model = new GameModel();
+const model = new StrategyGameModel();
 const renderer = new BattlefieldRenderer(view.elements.field, view.elements.fieldWrap);
 const buildInfo = window.BREACH_LINE_BUILD || { version: 'dev', commit: 'local' };
 const controller = new GameController(model, view, renderer, buildInfo);
