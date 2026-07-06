f7df1e5c423da485ad8479296a5bbc7b6a54c741
ajaxor <nathan.ferch@gmail.com>
2026-07-05 19:22:44 -0700

    Use battlefield renderer with conditional health bars

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import { GameController } from './backend/GameController.js';
-import { CanvasRenderer } from './frontend/CanvasRenderer.js';
+import { BattlefieldRenderer } from './frontend/BattlefieldRenderer.js';
 import { GameView } from './frontend/GameView.js';
 import { GameModel } from './model/GameModel.js';
 
 const view = new GameView();
 const model = new GameModel();
-const renderer = new CanvasRenderer(view.elements.field, view.elements.fieldWrap);
+const renderer = new BattlefieldRenderer(view.elements.field, view.elements.fieldWrap);
 const buildInfo = window.BREACH_LINE_BUILD || { version: 'dev', commit: 'local' };
 const controller = new GameController(model, view, renderer, buildInfo);
 controller.initialize();
