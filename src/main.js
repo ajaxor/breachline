0c480040f36d3cdfb573e87174a1ec1726e35dad
ajaxor <nathan.ferch@gmail.com>
2026-07-07 13:54:09 -0700

    Use deployment support range renderer

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 import { FlowGameController } from './backend/FlowGameController.js';
 import { validateGameData } from './data/validateGameData.js';
-import { BattlefieldRenderer } from './frontend/BattlefieldRenderer.js';
 import { CombatEventPresenter } from './frontend/CombatEventPresenter.js';
+import { DeploymentBattlefieldRenderer } from './frontend/DeploymentBattlefieldRenderer.js';
 import { FlowGameView } from './frontend/FlowGameView.js';
 import { StrategyGameModel } from './model/StrategyGameModel.js';
 
 validateGameData();
 const view = new FlowGameView();
 const model = new StrategyGameModel({ eventPresenter: new CombatEventPresenter() });
-const renderer = new BattlefieldRenderer(view.elements.field, view.elements.fieldWrap);
+const renderer = new DeploymentBattlefieldRenderer(view.elements.field, view.elements.fieldWrap);
 const buildInfo = window.BREACH_LINE_BUILD || { version: 'dev', commit: 'local' };
 const controller = new FlowGameController(model, view, renderer, buildInfo);
 controller.initialize();
