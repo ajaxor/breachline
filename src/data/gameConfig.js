b1c90dcc57eca5daecfaabc7fd928c36b9346c83
ajaxor <nathan.ferch@gmail.com>
2026-07-08 16:39:40 -0700

    Use tapered draft budget growth

--- src/data/gameConfig.js
+++ src/data/gameConfig.js
@@ -22,7 +22,8 @@ export const GAME_CONFIG = Object.freeze({
   structureBudgetBase: 80,
   structureBudgetStep: 40,
   startingDraftBudget: 120,
-  draftBudgetStep: 30,
+  draftBudgetStep: 40,
+  draftBudgetTaper: 2,
   playerZone: Object.freeze([0, 1, 2, 3, 4, 5, 6]),
   enemyZone: Object.freeze([7, 8, 9, 10, 11, 12, 13]),
 });
