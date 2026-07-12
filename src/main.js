b7b62fd341fc19bbe9d8c0d03c0c6fc860129483
ajaxor <nathan.ferch@gmail.com>
2026-07-11 18:44:35 -0700

    Wire combat presentation updates

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 import { FlowGameController } from './backend/FlowGameController.js';
 import { validateGameData } from './data/validateGameData.js';
-import { CombatEventPresenter } from './frontend/CombatEventPresenter.js';
-import { DeploymentBattlefieldRenderer } from './frontend/DeploymentBattlefieldRenderer.js';
+import { CenteredCombatEventPresenter } from './frontend/CenteredCombatEventPresenter.js';
+import { CombatRangeBattlefieldRenderer } from './frontend/CombatRangeBattlefieldRenderer.js';
 import { FlowGameView } from './frontend/FlowGameView.js';
 import { StrategyGameModel } from './model/StrategyGameModel.js';
 
 validateGameData();
 const view = new FlowGameView();
-const model = new StrategyGameModel({ eventPresenter: new CombatEventPresenter() });
-const renderer = new DeploymentBattlefieldRenderer(view.elements.field, view.elements.fieldWrap);
+const model = new StrategyGameModel({ eventPresenter: new CenteredCombatEventPresenter() });
+const renderer = new CombatRangeBattlefieldRenderer(view.elements.field, view.elements.fieldWrap);
 const buildInfo = window.BREACH_LINE_BUILD || { version: 'dev', commit: 'local' };
 const controller = new FlowGameController(model, view, renderer, buildInfo);
 controller.initialize();
