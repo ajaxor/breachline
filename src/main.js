7a2ecaabfd45d516d68158c1ea4eddfc9a62b887
ajaxor <nathan.ferch@gmail.com>
2026-07-17 16:11:02 -0700

    Use finale fade battlefield renderer

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 import { FlowGameController } from './backend/FlowGameController.js';
 import { validateGameData } from './data/validateGameData.js';
 import { CenteredCombatEventPresenter } from './frontend/CenteredCombatEventPresenter.js';
-import { CombatRangeBattlefieldRenderer } from './frontend/CombatRangeBattlefieldRenderer.js';
 import { FileTrackAudioDirector } from './frontend/FileTrackAudioDirector.js';
+import { FinaleFadeBattlefieldRenderer } from './frontend/FinaleFadeBattlefieldRenderer.js';
 import { FlowGameView } from './frontend/FlowGameView.js';
 import { StrategyGameModel } from './model/StrategyGameModel.js';
 
 validateGameData();
 const view = new FlowGameView();
 const model = new StrategyGameModel({ eventPresenter: new CenteredCombatEventPresenter() });
-const renderer = new CombatRangeBattlefieldRenderer(view.elements.field, view.elements.fieldWrap);
+const renderer = new FinaleFadeBattlefieldRenderer(view.elements.field, view.elements.fieldWrap);
 const audioDirector = new FileTrackAudioDirector();
 const buildInfo = window.BREACH_LINE_BUILD || { version: 'dev', commit: 'local' };
 const controller = new FlowGameController(model, view, renderer, buildInfo, { audioDirector });
